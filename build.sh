350a6a93f78e7666a5318e414c7eabe3ab7a5191
BazzoFX <39503563+bazzofx@users.noreply.github.com>
2024-11-11 02:04:32 +0000

    Use this to build the binary
    Use this to build

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+go build -o pbfff main.go
+echo "After building, you will need to move the binary to your /usr/bin or alternatively move to ~/go/bin and add the ~/go/bin/ to your environment \$PATH"
